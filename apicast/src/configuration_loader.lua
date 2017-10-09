fe4d32f52b4eab9eeb9f2ca34bb1527e85f2f3ad
Michal Cichra <michal@cichra.cz>
2017-10-09 12:16:39 +0200

    prototype basic policy chain

--- apicast/src/configuration_loader.lua
+++ apicast/src/configuration_loader.lua
@@ -54,7 +54,7 @@ local function ttl()
 end
 
 function _M.global(contents)
-  local module = require('module')
+  local module = require('policy_chain')
 
   return _M.configure(module.configuration, contents)
 end
